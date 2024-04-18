62f0970834d1a7f312a384b848d6fa2e0c462c5d
RalucaDavid <117584603+RalucaDavid@users.noreply.github.com>
2024-04-18 18:46:47 +0300

    Implement API to show the examples.

--- Flight-Booking/flight-booking.client/src/app/search-flights/search-flights.component.ts
+++ Flight-Booking/flight-booking.client/src/app/search-flights/search-flights.component.ts
@@ -1,47 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { FlightRm } from '../api/models';
+import { FlightService } from './../api/services/flight.service';
 
 @Component({
   selector: 'app-search-flights',
   templateUrl: './search-flights.component.html',
   styleUrl: './search-flights.component.css'
 })
-export class SearchFlightsComponent{
+export class SearchFlightsComponent {
 
-  searchResult: FlightRm[] = [
-    {
-      airline: "American Airlines",
-      remainingNumberOfSeats: 500,
-      departure: { time: Date.now().toString(), place: "Los Angeles" },
-      arrival: { time: Date.now().toString(), place: "Istanbul" },
-      price: "350",
-    },
-    {
-      airline: "Tarom",
-      remainingNumberOfSeats: 50,
-      departure: { time: Date.now().toString(), place: "Berlin" },
-      arrival: { time: Date.now().toString(), place: "Bucharest" },
-      price: "450",
-    },
-    {
-      airline: "Wizz Air",
-      remainingNumberOfSeats: 367,
-      departure: { time: Date.now().toString(), place: "Paris" },
-      arrival: { time: Date.now().toString(), place: "Rome" },
-      price: "600",
-    }
-  ]
+  searchResult: FlightRm[] = []
 
-}
+  constructor(private flightService: FlightService) {
 
-export interface FlightRm {
-  airline: string;
-  arrival: TimePlaceRm;
-  departure: TimePlaceRm;
-  price: string;
-  remainingNumberOfSeats: number;
-}
+  }
 
-export interface TimePlaceRm{
-  place: string;
-  time: string;
+  search() {
+    this.flightService.flightGet({})
+      .subscribe(r => this.searchResult = r)
+  }
 }
