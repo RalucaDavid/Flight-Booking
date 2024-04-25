d326eeec164095214abbaae44c321fd9cf254568
RalucaDavid <117584603+RalucaDavid@users.noreply.github.com>
2024-04-25 15:01:36 +0300

    Add validation for the Dtos.
    Add validaton for BookDto and NewPassengerDto.

--- Flight-Booking/Flight-Booking.Server/Dtos/BookDto.cs
+++ Flight-Booking/Flight-Booking.Server/Dtos/BookDto.cs
@@ -1,9 +1,11 @@
-﻿namespace Flight_Booking.Server.Dtos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Flight_Booking.Server.Dtos
 {
     public record BookDto(
-        Guid FlightId,
-        string PassengerEmail,
-        byte NumberOfSeats
+        [Required] Guid FlightId,
+        [Required][EmailAddress][StringLength(100, MinimumLength = 3)] string PassengerEmail,
+        [Required][Range(1,254)] byte NumberOfSeats
         );
     
 }
