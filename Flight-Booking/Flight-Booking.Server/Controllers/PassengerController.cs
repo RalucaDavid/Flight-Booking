b3bcc82c23aa8ee873e9bf5ce8d6d82246f53e1d
RalucaDavid <117584603+RalucaDavid@users.noreply.github.com>
2024-04-26 19:24:26 +0300

    Getting ready for database.
    Add the Entities class.
    Refactor the entities from the controllers.
    Injecting the Entities Singleton.
    Seeding data in program cs.

--- Flight-Booking/Flight-Booking.Server/Controllers/PassengerController.cs
+++ Flight-Booking/Flight-Booking.Server/Controllers/PassengerController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Flight_Booking.Server.Dtos;
 using Flight_Booking.Server.ReadModels;
 using Flight_Booking.Server.Domain.Entities;
+using Flight_Booking.Server.Data;
 
 namespace Flight_Booking.Server.Controllers
 {
@@ -10,7 +11,12 @@ namespace Flight_Booking.Server.Controllers
     [ApiController]
     public class PassengerController : ControllerBase
     {
-        static private IList<Passenger> Passengers = new List<Passenger>();
+        private readonly Entities _entities;
+
+        public PassengerController(Entities entities)
+        {
+            _entities = entities;
+        }
 
         [HttpPost]
         [ProducesResponseType(201)]
@@ -18,20 +24,20 @@ namespace Flight_Booking.Server.Controllers
         [ProducesResponseType(500)]
         public IActionResult Register(NewPassengerDto dto)
         {
-            Passengers.Add(new Passenger(
+            _entities.Passengers.Add(new Passenger(
                 dto.Email,
                 dto.FirstName,
                 dto.LastName,
                 dto.Gender
                 ));
-            System.Diagnostics.Debug.WriteLine(Passengers.Count);
+            System.Diagnostics.Debug.WriteLine(_entities.Passengers.Count);
             return CreatedAtAction(nameof(Find), new { email = dto.Email }, dto);
         }
 
         [HttpGet("{email}")]
         public ActionResult<PassengerRm> Find(string email)
         {
-            var passenger = Passengers.FirstOrDefault(p => p.Email == email);
+            var passenger = _entities.Passengers.FirstOrDefault(p => p.Email == email);
 
             if(passenger == null)
             {
---
+++ Flight-Booking/Flight-Booking.Server/Data/Entities.cs
@@ -0,0 +1,11 @@
+﻿using Flight_Booking.Server.Domain.Entities;
+using System;
+
+namespace Flight_Booking.Server.Data
+{
+    public class Entities
+    {
+        public IList<Passenger> Passengers = new List<Passenger>();
+        public List<Flight> Flights = new List<Flight>();
+    }
+}
