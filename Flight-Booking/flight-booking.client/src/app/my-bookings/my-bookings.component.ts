33a493bd1681ea70c359eb278d73001a52047205
RalucaDavid <117584603+RalucaDavid@users.noreply.github.com>
2024-04-27 17:09:32 +0300

    Add Route Guards.
    Add the Auth Guard.
    Use AuthGuard where the authentication is required.
    Add AutoRedirect to requested url after login.

--- Flight-Booking/flight-booking.client/src/app/my-bookings/my-bookings.component.ts
+++ Flight-Booking/flight-booking.client/src/app/my-bookings/my-bookings.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { BookingRm, BookDto } from '../api/models';
 import { BookingService } from './../api/services/booking.service';
 import { AuthService } from './../auth/auth.service';
-import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-my-bookings',
@@ -13,12 +12,9 @@ export class MyBookingsComponent implements OnInit {
   bookings!: BookingRm[];
   constructor(private bookingService: BookingService,
     private authService: AuthService,
-    private router: Router
   ) { }
 
   ngOnInit(): void {
-    if (!this.authService.currentUser?.email)
-      this.router.navigate(['/register-passenger']);
     this.bookingService.listBooking({ email: this.authService.currentUser?.email ?? ' ' })
       .subscribe(r => this.bookings = r, this.handleError);
   }
